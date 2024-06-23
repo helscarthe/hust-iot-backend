493aedead136f75aa282c53e82da139142514c2f
helscarthe <nguyenduytuan52@gmail.com>
2024-06-23 16:39:18 +0700

    add commands for iot side

---
+++ routes/command.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const Model = require("../models/model.js.example");
+const router = express.Router();
+const client = require("../mqtt");
+
+router.get("/:value", async (req, res) => {
+  try {
+    const data = '{"limit":' + req.params.value + '}';
+    client.publish("output", data);
+    res.status(200).send(data + " sent to broker");
+  } catch (err) {
+    res.status(500).json({ message: "Data not found", error: err });
+  }
+})
+
+module.exports = router;
